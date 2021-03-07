56d095a8c7f9161bd651292ccbcef21c545e414b
YunusEMRE <53182424+emre-23@users.noreply.github.com>
2021-03-07 14:35:47 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	var slices = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	for i, slice := range slices {
-		if i%2 == 0 {
-			fmt.Println(slice, "is even")
-		} else {
-			fmt.Println(slice, "is odd")
-		}
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
+}
 
-	}
+
+	
 }
